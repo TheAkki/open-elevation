9dba8fdc35edd6fcc670f41d630107cec32d5b31
TheAkki <theakki@mailbox.org>
2019-11-05 20:32:08 +0100

    Skip allready processed files. Use unly 1arc file

--- create-tiles-inFolder.sh
+++ create-tiles-inFolder.sh
@@ -4,6 +4,8 @@ xtiles=$2
 ytiles=$3
 
 for filename in $folder/*.tif; do
-    CMD="./create-tiles.sh $filename $xtiles $ytiles"
-    `$CMD`
+    if echo $filename | grep -Eq '[ns][0-9][0-9]_[ew][0-9][0-9][0-9]_[0-9a-zA-Z]*_v[0-9].tif'; then
+        CMD="./create-tiles.sh $filename $xtiles $ytiles"
+        `$CMD`
+    fi
 done
